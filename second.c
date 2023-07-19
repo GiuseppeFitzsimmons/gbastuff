161af27110eb7c1a290edeca8742117f64e4812e
GiuseppeFitzsimmons <fitzsimmonsgiuseppe@gmail.com>
2023-07-19 21:32:50 +0200

    initial commit

---
+++ second.c
@@ -0,0 +1,23 @@
+#include "toolbox.h"
+#include <stdio.h>
+
+// extra stuff, also in tonc_video.h
+#define M3_WIDTH    SCREEN_WIDTH
+// typedef for a whole mode3 line
+typedef COLOR       M3LINE[M3_WIDTH];
+// m3_mem is a matrix; m3_mem[y][x] is pixel (x,y)
+#define m3_mem    ((M3LINE*)MEM_VRAM)
+
+int main()
+{
+    REG_DISPCNT= DCNT_MODE3 | DCNT_BG2;
+
+    m3_mem[80][120]= CLR_RED;
+    m3_mem[80][136]= CLR_LIME;
+    m3_mem[96][120]= CLR_BLUE;
+
+    printf("Hello, World!");
+
+    while(1);
+    return 0;
+}
