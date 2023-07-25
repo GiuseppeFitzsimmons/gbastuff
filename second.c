cc50cb9083a12fc038c159d94b7b9ec5c5ef8301
GiuseppeFitzsimmons <fitzsimmonsgiuseppe@gmail.com>
2023-07-25 20:40:41 +0200

    feat: understood bitwise operations and memory offsets, bitmap chapter 2big42nite

--- second.c
+++ second.c
@@ -1,23 +1,65 @@
 #include "toolbox.h"
-#include <stdio.h>
 
 // extra stuff, also in tonc_video.h
-#define M3_WIDTH    SCREEN_WIDTH
+#define M3_WIDTH SCREEN_WIDTH
 // typedef for a whole mode3 line
-typedef COLOR       M3LINE[M3_WIDTH];
+typedef COLOR M3LINE[M3_WIDTH];
 // m3_mem is a matrix; m3_mem[y][x] is pixel (x,y)
-#define m3_mem    ((M3LINE*)MEM_VRAM)
+#define m3_mem ((M3LINE *)MEM_VRAM)
+
+// void vid_vsync()
+// {
+//     while (REG_VCOUNT >= 160)
+//         ; // wait till VDraw
+//     while (REG_VCOUNT < 160)
+//         ; // wait till VBlank
+// }
 
 int main()
 {
-    REG_DISPCNT= DCNT_MODE3 | DCNT_BG2;
+    REG_DISPCNT = DCNT_MODE3 | DCNT_BG2;
+    // 0x0003 > 0x0000000000000011
+    // 0x0400 > 0x0000100000000000
+    //   |    > 0x0000100000000011
+    int x, y;
 
-    m3_mem[80][120]= CLR_RED;
-    m3_mem[80][136]= CLR_LIME;
-    m3_mem[96][120]= CLR_BLUE;
+    for (x = 1; x < SCREEN_HEIGHT; ++x)
+    {
+        y = 10 * x;
+        m3_mem[x][y] = CLR_BLUE;
+    }
+    for (y = 1; y < SCREEN_WIDTH * 2; ++y)
+    {
+        x = 10 * y;
+        m3_mem[x][y] = CLR_LIME;
+    }
+    for (y = SCREEN_WIDTH / 2; y < SCREEN_WIDTH; ++y)
+    {
+        x = 10 * y;
+        m3_mem[x][y] = CLR_MAG;
+    }
 
-    printf("Hello, World!");
-
-    while(1);
+    while (1)
+        ;
     return 0;
 }
+
+// H
+// for (i = 120; i < 127; ++i)
+// {
+//     m3_mem[80][i] = CLR_BLUE;
+// }
+// for (i = 72; i < 88; ++i)
+// {
+//     m3_mem[i][120] = CLR_CYAN;
+// }
+// for (i = 72; i < 88; ++i)
+// {
+//     m3_mem[i][126] = CLR_RED;
+// }
+
+// // I
+// for (i = 72; i < 88; ++i)
+// {
+//     m3_mem[i][136] = CLR_CYAN;
+// }
